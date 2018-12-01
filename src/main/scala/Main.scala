4f484b12592265895b7935f2cfc840cfbec7884b
Anthony Lam <thelamacmdr@gmail.com>
2018-12-01 10:13:12 -0800

    Add Day1 solutions

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,38 @@
+object Main extends App {
+  override def main(args: Array[String]): Unit = {
+    println(Day1.solutionPartOne)
+    println(Day1.solutionPartTwo)
+  }
+}
+
+object Day1 {
+  val pairs = "([+-])([0-9]+)".r
+  val input = scala.io.Source.fromFile("input.txt").mkString.split('\n')
+
+  val matcher = (res: Int, line: String) => {
+      line match {
+        case pairs("+", value) => res + value.toInt
+        case pairs("-", value) => res - value.toInt
+        case _ => res
+      }
+  }
+
+  def solutionPartOne = {
+    input.foldLeft(0)(matcher)
+  }
+
+  def solutionPartTwo = {
+    var seen = new scala.collection.mutable.HashSet[Int]
+    Iterator.continually(input).flatten
+      .scanLeft(0)(matcher)
+      .filter(v => {
+        if (seen.contains(v)) {
+          true
+        } else {
+          seen += v
+          false
+        }
+      }).next
+  }
+
+}
