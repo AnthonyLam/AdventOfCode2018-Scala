944d133998e2b23dfef955fb10f9658fe0692604
Anthony Lam <thelamacmdr@gmail.com>
2018-12-17 23:04:07 -0800

    Day 4 submission: Finally got around to completing this.
    Something I discovered was the hardest part is manipulating
    the data into a datastructure most
    readily used.

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -1,33 +1,57 @@
 // https://adventofcode.com/2018/day/4
 
-import scala.collection.mutable.PriorityQueue
-import scala.collection.mutable.Map
+import scala.collection.mutable.HashMap
 
 object Day4 {
   // Match yields (month, day, hour, minute, status, Maybe Guard#)
   val regex = "\\[1518-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\\] (wakes|falls|Guard #([0-9]+))".r.unanchored
-  val input = scala.io.Source.fromFile("input4.txt").mkString.lines
+  val input = scala.io.Source.fromFile("input4.txt").mkString.split('\n')
+
+
+  // Generate a mapping of { Guard# -> minutes the guard slept}
+  def guardStore = {
+    val store = HashMap[Int, Seq[Int]]()
+    var curGuard = 0
+    var startMinute = 0
+    
+    input
+    .map({ case regex(mon, day, hr, min, status, guard) => (mon, day, hr, min, status, guard) }) // regex parse
+    .sorted  // sort tuples to get chronological order
+    .foreach(_ match {
+      case (_,_,_, min, "wakes", null) => {                           // Match case where the guard wakes up and we can calculate how many minutes he slept
+        var cur = store.getOrElseUpdate(curGuard, Seq[Int]())
+        store.put(curGuard, cur ++ (startMinute until min.toInt))
+      }
+      case (_,_,_, min, "falls", null) => startMinute = min.toInt     // Match case where guard falls asleep
+      case (_,_,_,_,_, guard) => curGuard = guard.toInt               // Start the day for this guard
+    })
+    store
+  }
 
   def solutionPartOne = {
-    // A map to store our (Date : PriorityQueue)
-    val store = Map[Tuple2[Int, Int], PriorityQueue[Tuple2[Int, String]]]()
+    val store = guardStore
 
-    // Separate each day out into buckets, and sort within those days with a PriorityQueue
-    input.map(_ match {
-      case regex(month, day, hour, minute, status, null) => ((month.toInt, day.toInt), (minute.toInt, status))
-      case regex(month, day, hour, minute, status, guard_num) => {
-        // This fool started work early. He's not technicallly working till the next day. (I'm hoping no one does this at the end of the month)
-        if(hour == "23")
-          ((month.toInt, day.toInt+1), (0, guard_num))
-        else
-          ((month.toInt, day.toInt), (minute.toInt, guard_num))
-      }
-    })
-    .foreach( {case (date, status) => 
-      store.getOrElseUpdate(date, PriorityQueue[Tuple2[Int, String]]()).enqueue(status)
-    })
+    // get the guard who slept the most minutes
+    var maxGuard = store
+      .map({ case (k,v) => (k, v.size) })
+      .maxBy({ case (k,v) => v })
 
-//    store.toIterator.map()
-    ""
+    // Get the minute the guard slept the most
+    var minute = store
+                  .getOrElse(maxGuard._1, List())
+                  .groupBy(identity).mapValues(_.size)
+                  .maxBy({case (k,v) => v})
+    maxGuard._1 * minute._1
+  }
+
+  def solutionPartTwo = {
+    val store = guardStore
+    var result = store.map({
+      // For each guard, find the minute each slept the most
+      case (k,v) => (k, v.groupBy(identity).mapValues(_.size).maxBy(_._2))
+    })
+    // Find the guard who consistently slept the most at a particular minute
+    .maxBy(_._2._2)
+    result._1 * result._2._1
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,11 +4,15 @@ object Main extends App {
     // println(Day1.solutionPartTwo)
 
     //println("Day 2 Part one solution: ", Day2.solutionPartOne)
-    //println("Day 2 Part two solution: ", Day2.solutionPartTwo)
+    // println("Day 2 Part two solution: ", Day2.solutionPartTwo)
 
     // println(" Day 3 Part one solution: ", Day3.solutionPartOne)
 
-    println(" Day 5 Part one solution: ", Day5.solutionPartOne)
-    println(" Day 5 Part two solution: ", Day5.solutionPartTwo)
+    println(" Day 4 Part one solution: ", Day4.solutionPartOne)
+    println(" Day 4 Part two solution: ", Day4.solutionPartTwo)
+
+    // println(" Day 5 Part one solution: ", Day5.solutionPartOne)
+    // println(" Day 5 Part two solution: ", Day5.solutionPartTwo)
+
   }
 }
